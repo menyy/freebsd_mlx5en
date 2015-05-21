11298dfeb38b347db13192b34089f37802f6e816
Hans Petter Selasky <hanss@mellanox.com>
2015-05-21 15:52:59 +0200

    Fix return code in error case.
    Signed-off-by: Hans Petter Selasky <hanss@mellanox.com>

--- freebsd/ofed/drivers/net/mlx5/mlx5_en_main.c
+++ freebsd/ofed/drivers/net/mlx5/mlx5_en_main.c
@@ -773,7 +773,8 @@ mlx5e_create_sq(struct mlx5e_channel *c,
 	sq->uar_map = sq->uar.map;
 	sq->bf_buf_size = (1 << MLX5_CAP_GEN(mdev, log_bf_reg_size)) / 2;
 
-	if (mlx5e_alloc_sq_db(sq))
+	err = mlx5e_alloc_sq_db(sq);
+	if (err)
 		goto err_sq_wq_destroy;
 
 	sq->pdev = c->pdev;
