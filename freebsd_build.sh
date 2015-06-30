460b711cf829a3353992b0b079f4d918bb7bb69d
Mark Bloch <markb@mellanox.com>
2015-06-30 08:17:02 +0300

    Changes needed for public use.
    Chnages to the code so it could be compiled and used outside of Mellanox.
    
    Signed-off-by: Mark Bloch <markb@mellanox.com>

--- freebsd_build.sh
+++ freebsd_build.sh
@@ -5,7 +5,6 @@ SRC="$1"
 [ -z "$SRC" ] && SRC="/usr/src"
 
 echo "The FreeBSD source tree is at $SRC ..."
-bmake -m $SRC/share/mk SYSDIR=$SRC/sys -C freebsd/modules $LRO generate
 bmake -m $SRC/share/mk SYSDIR=$SRC/sys -C freebsd/modules $LRO clean cleandepend
 bmake -m $SRC/share/mk SYSDIR=$SRC/sys -C freebsd/modules $LRO depend
 bmake -m $SRC/share/mk SYSDIR=$SRC/sys -C freebsd/modules $LRO all -j12
